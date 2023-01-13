b543fefc6993137a457b617da63094346baf584d
daisylau1118 <daisywklau@hotmail.com>
2023-01-13 16:46:42 -0700

    added line so script can download and print from GitHub

--- lab1.py
+++ lab1.py
@@ -2,3 +2,4 @@ import requests
 
 print(requests.__version__)
 print(requests.get("http://www.google.com/"))
+print(requests.get("https://raw.githubusercontent.com/daisylau1118/CMPUT404-Lab1/main/lab1.py").text)
